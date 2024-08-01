2d5b51d24b6f8fa448f30ef68d99b6112b6588b9
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-01 22:35:03 +0100

    refactor(tests): extract common code into load function
    The repetitive code for loading and preprocessing Svelte files in
    tests has been extracted into a separate async function named 'load'.
    This makes the test cases cleaner and easier to read.

--- mod_test.ts
+++ mod_test.ts
@@ -3,37 +3,34 @@ import { assertSnapshot } from "@std/testing/snapshot";
 import { preprocess } from "svelte/compiler";
 import { importCSSPreprocess } from "./mod.ts";
 
+async function load(_filename: string) {
+  const filename = join(
+    import.meta.dirname as string,
+    _filename,
+  );
+
+  const source = Deno.readTextFileSync(filename);
+  const { code } = await preprocess(
+    source,
+    [importCSSPreprocess()],
+    { filename },
+  );
+
+  return code;
+}
+
 Deno.test(
   "example svelte main",
   async function (t) {
-    const filename = join(
-      import.meta.dirname as string,
-      "./test_project/Main.svelte",
-    );
-    const source = Deno.readTextFileSync(filename);
-    const { code } = await preprocess(
-      source,
-      [importCSSPreprocess()],
-      { filename },
-    );
-
+    const code = await load("./test_project/Main.svelte");
     await assertSnapshot(t, code);
   },
 );
+
 Deno.test(
   "example svelte partial",
   async function (t) {
-    const filename = join(
-      import.meta.dirname as string,
-      "./test_project/Partial.svelte",
-    );
-    const source = Deno.readTextFileSync(filename);
-    const { code } = await preprocess(
-      source,
-      [importCSSPreprocess()],
-      { filename },
-    );
-
+    const code = await load("./test_project/Partial.svelte");
     await assertSnapshot(t, code);
   },
 );
